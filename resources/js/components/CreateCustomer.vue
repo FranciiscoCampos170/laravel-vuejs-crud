f1af5dcae8d3ec3f385b525ea86e47d90643c2b6
Francisco Campos <franciiscocampos170@gmail.com>
2020-04-13 10:55:54 +0100

    form label's edited

--- resources/js/components/CreateCustomer.vue
+++ resources/js/components/CreateCustomer.vue
@@ -3,13 +3,13 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Criar Novos Clientes</div>
+                    <div class="card-header">Insert New Customers</div>
 
                     <div class="card-body">
                        <form method="POST">
 
                            <div class="form-group">
-                                <label for="name">Nome Completo</label>
+                                <label for="name">Full Name</label>
                                 <input type="text" class="form-control" id="name" name="name" v-model="name">
                             </div>
                             <div class="form-group">
@@ -17,11 +17,11 @@
                                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" v-model="email">
                             </div>
                             <div class="form-group">
-                                <label for="role">Cargo</label>
+                                <label for="role">Role</label>
                                 <input type="text" class="form-control" id="role" name="role" v-model="role">
                             </div>
                             <div class="form-group">
-                                <label for="company">Empresa</label>
+                                <label for="company">Company</label>
                                 <input type="text" class="form-control" id="company" name="company" v-model="company">
                             </div>
                             <button type="button" class="btn btn-primary" v-on:click="addCustomer()">Salvar</button>
